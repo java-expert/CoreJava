22232b006214d4ed22f4a2d097325f46328340fe
java-expert <jagjotmca@yahoo.co.in>
2016-08-31 23:59:08 +0530

    exception

---
+++ src/exceptionHandling/MainClass.java
@@ -0,0 +1,24 @@
+package exceptionHandling;
+import java.io.IOException; 
+
+class MainClass{  
+  void m()throws IOException{  
+    throw new IOException("device error");//checked exception  
+  }  
+  
+  void n()throws IOException{  
+    m();  
+  }  
+  void p(){  
+   try{  
+    n();  
+   }catch(Exception e){
+	   System.out.println("exception handled");
+	   }  
+  }  
+  public static void main(String args[]){  
+	  MainClass obj=new MainClass();  
+   obj.p();  
+   System.out.println("normal flow...");  
+  }  
+} 
