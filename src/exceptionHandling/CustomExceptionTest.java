a41fa66636ea816cedebc9d8893c298e7b72666c
java-expert <jagjotmca@yahoo.co.in>
2016-09-01 00:12:22 +0530

    exception

--- src/exceptionHandling/CustomExceptionTest.java
+++ src/exceptionHandling/CustomExceptionTest.java
@@ -1,5 +1,19 @@
 package exceptionHandling;
 
 public class CustomExceptionTest {
+	void display() throws MyException{
+		if(2>1)
+			throw new MyException("2 is greater than 1 and i dont like it :)");
+		
+	}
+	public static void main(String[] args) throws MyException {
+		CustomExceptionTest c=new CustomExceptionTest();
+		try{
+		c.display();
+		}catch(Exception e){
+			System.out.println("exception occured :: "+e);
+		}
+	
+	}
 
 }
