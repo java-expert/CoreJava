982ddd045ae38a53dc8844b6cfbebbc49d77ac8b
java-expert <jagjotmca@yahoo.co.in>
2016-10-23 16:19:11 +0530

    immutable

---
+++ src/immutable/MyImmutableClass.java
@@ -0,0 +1,21 @@
+package immutable;
+/* Immutable Class 
+ * Final Class
+ * Final variables
+ * No Setter method
+ */
+public final class MyImmutableClass {
+final String str;
+public MyImmutableClass(String str){
+	this.str=str;
+}
+
+public String getStr(){
+	return str;
+}
+
+public static void main(String[] args) {
+	MyImmutableClass  obj1=new MyImmutableClass("custom string");
+	obj1.getStr();
+}
+}
